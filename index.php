292543f911a6086a763411879de323442498d0fd
annalisadamore91 <annalisadamore91@libero.it>
2017-03-16 22:59:41 +0000

    modifica2

--- index.php
+++ index.php
@@ -4,7 +4,28 @@
         
     </head>
     <body>
-        <?php for($i = 0; $i<4;$i++){
+        <?php
+        $myString = "pepe";
+        $myInteger = 12;
+        $myFloat = 34.23;
+        $myBoolean = true;
+        //$myArray = array(2,3,4,5,77,"2323",null);
+        $myArray = array (
+            name=>"pepe",
+            lastname =>"pippo",
+            name => "tttt"
+            );
+        $myFirstNull = NULL;
+        //$myObject = new Object();
+          print(myArray);
+      
+        if($myBoolean ==true)
+        {
+            if($myString!== 'pepe')
+        
+        
+        
+         for($i = 0; $i<4;$i++){
             echo "<p>hello world</p> \n";
         }
         ?>
